d3a4c0337a8af6863272256d3d1212cacae23d37
EhsanEhrari <ehrarie@gmail.com>
2016-02-17 21:40:03 -0800

    Create Article Table

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
     root 'welcome#index'
-    get 'welcome/about', to:'welcome#about'
+    get 'about', to:'welcome#about'
+    resources:articles
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
---
+++ db/migrate/20160216172328_add_descreption_to_articles.rb
@@ -0,0 +1,7 @@
+class AddDescreptionToArticles < ActiveRecord::Migration
+  def change
+    add_column :articles, :descreption, :text
+    add_column :articles, :created_at, :datetime
+    add_column :articles, :updated_at, :datetime
+  end
+end
